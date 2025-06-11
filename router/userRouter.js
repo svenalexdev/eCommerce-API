1d351145de4d6259411094a2f2a63eebf8af5784
Ankita Malani <ankitamalani2021@gmail.com>
2025-06-11 17:53:58 +0200

    new add

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,5 +1,4 @@
 import { Router } from "express";
-// import validateBody from '../middleware/validateBody';
 //import {userSchema} from '../zod/schemas.js';
 import { getUsers,createUser,getUserById,updateUser,deleteUser } from "../controllers/users.js";
 
