27ab961bfab0419e6e87a8a4682ed6c8ef6fddde
Sven Kaack <sven.kaack@gmx.de>
2025-06-12 00:07:18 +0200

    created order model

---
+++ models/Order.js
@@ -0,0 +1,28 @@
+import { Schema, model } from 'mongoose';
+
+const orderSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  products: [
+    {
+      productId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+      },
+      quantity: {
+        type: Number,
+        required: [true, 'Quantity is required']
+      }
+    }
+  ],
+  total: {
+    type: Number,
+    required: true
+  }
+});
+
+export default model('Order', orderSchema);
