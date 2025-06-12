34d9dbddf39aed69927754a0f1637787e6f0b632
Sven Kaack <sven.kaack@gmx.de>
2025-06-12 11:34:10 +0200

    added order logic, controllers, router

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import './db/index.js';
 import userRouter from './routers/userRouter.js';
 import productRouter from './routers/productRouter.js';
 import categoryRouter from './routers/categoryRouter.js';
+import orderRouter from './routers/orderRouter.js';
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -10,9 +11,10 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 
 app.use('/categories', categoryRouter);
-app.use('/users',userRouter);
-app.use('/products',productRouter);
+app.use('/orders', orderRouter);
+app.use('/users', userRouter);
+app.use('/products', productRouter);
 
-app.listen(port,() =>{
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
--- models/Product.js
+++ models/Product.js
@@ -1,11 +1,11 @@
-import { Schema, model } from "mongoose";
+import { Schema, model } from 'mongoose';
 
 const productSchema = new Schema({
-  name: { type: String, required: [true, "Product name is requires"] },
-  description: { type: String, requires: [true, "Description is required"] },
-  price: { type: Number, required: [true, "Price is required"] },
+  name: { type: String, required: [true, 'Product name is requires'] },
+  description: { type: String, requires: [true, 'Description is required'] },
+  price: { type: Number, required: [true, 'Price is required'] },
   categoryId: { type: Number, required: true, unique: true, integer: true },
-  createAr: { type: Date, default: Date.now },
+  createAr: { type: Date, default: Date.now }
 });
 
-export default model('Peoduct',productSchema);
+export default model('Product', productSchema);
